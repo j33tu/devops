142741caec9e4cc4547af71389ef4df9c49d3a27
Jitendra Singh <admin@jitenrdasingh.net>
2026-02-11 12:35:05 +0530

    deplying  mysql db in azure to east us

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -17,5 +17,13 @@ module "network_rg" {
 
   location = "West US"
 }
-
+module "mysql_db" {
+  source              = "./modules/mysql"
+  server_name         = "g2-prod-mysql-server"
+  resource_group_name = "compute" # Use one created by your RG module
+  location            = "eastus"
+  admin_username      = "g2admin"
+  admin_password      = var.mysql_password # Pass this from GitLab CI Secrets
+  db_name             = "app_db"
+}
 
