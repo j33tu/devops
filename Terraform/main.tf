9229235052878aebc909ed803f1de707e1445ebc
Jitendra Singh <admin@jitenrdasingh.net>
2026-02-11 11:25:47 +0530

    removing  rg into azure

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -29,35 +29,3 @@ module "app_rga" {
   rg_name  = "rg-dev-applications"
   location = "East US"
 }
-module "app_rga12" {
-  source = "./modules/rg"
-
-  rg_name  = "rg-dev1-applications"
-  location = "East US"
-}
-
-module "app_rga13" {
-  source = "./modules/rg"
-
-  rg_name  = "rg-dev2-applications"
-  location = "East US"
-}
-module "app_rga14" {
-  source = "./modules/rg"
-
-  rg_name  = "rg-g2-applications"
-  location = "East US"
-}
-module "app_rga15" {
-  source = "./modules/rg"
-
-  rg_name  = "rg-g212-applications"
-  location = "East US"
-}
-
-module "app_rga16" {
-  source = "./modules/rg"
-
-  rg_name  = "rg-g2-vdi"
-  location = "East US"
-}
