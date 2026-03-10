a8a5ad2b1329ee971df9da32b4805357d996a145
Jitendra Singh <admin@jitenrdasingh.net>
2026-03-10 21:53:11 +0530

    adding cdn to azure

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -21,5 +21,73 @@ module "mysql_db" {
 }
 
 
+# 1. Resource Group
+resource "azurerm_resource_group" "cdn_rg" {
+  name     = "rg-cricket-cdn-prod"
+  location = "West US 2"
+}
+
+# 2. Storage Account (The Origin)
+resource "azurerm_storage_account" "website_storage" {
+  name                     = "stparakramwebprod"
+  resource_group_name      = azurerm_resource_group.cdn_rg.name
+  location                 = azurerm_resource_group.cdn_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_account_static_website" "website_static" {
+  storage_account_id = azurerm_storage_account.website_storage.id
+  index_document     = "index.html"
+}
+
+# 3. Azure Front Door Profile (The CDN)
+resource "azurerm_cdn_frontdoor_profile" "main" {
+  name                = "afd-parakram-global"
+  resource_group_name = azurerm_resource_group.cdn_rg.name
+  sku_name            = "Standard_AzureFrontDoor"
+}
+
+# 4. Front Door Endpoint
+resource "azurerm_cdn_frontdoor_endpoint" "endpoint" {
+  name                     = "parakram-live"
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.main.id
+}
+
+# 5. Origin Group and Origin
+resource "azurerm_cdn_frontdoor_origin_group" "og" {
+  name                     = "og-storage"
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.main.id
+  load_balancing {}
+  health_probe {
+    interval_in_seconds = 100
+    protocol            = "Https"
+  }
+}
+
+resource "azurerm_cdn_frontdoor_origin" "storage_origin" {
+  name                           = "origin-storage"
+  cdn_frontdoor_origin_group_id  = azurerm_cdn_frontdoor_origin_group.og.id
+  host_name                      = azurerm_storage_account.website_storage.primary_web_host
+  certificate_name_check_enabled = true
+}
+
+# 6. Route (Mapping Endpoint to Origin)
+resource "azurerm_cdn_frontdoor_route" "route" {
+  name                          = "default-route"
+  cdn_frontdoor_endpoint_id     = azurerm_cdn_frontdoor_endpoint.endpoint.id
+  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.og.id
+  cdn_frontdoor_origin_ids      = [azurerm_cdn_frontdoor_origin.storage_origin.id]
+  supported_protocols           = ["Http", "Https"]
+  patterns_to_match             = ["/*"]
+  forwarding_protocol           = "HttpsOnly"
+
+  # Crucial for CDN: Enable Caching
+  cache {
+    query_string_caching_behavior = "IgnoreQueryString"
+    compression_enabled           = true
+  }
+}
+
 
 
