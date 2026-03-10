70d7c20f2c4113aa1716481df8d0d1c3a70be7cf
Jitendra Singh <admin@jitenrdasingh.net>
2026-03-10 22:21:56 +0530

    adding cdn to azure

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,8 +1,12 @@
-provider "azurerm" {
-  features {}
-}
-
+# --- Provider & Backend Configuration ---
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0" 
+    }
+  }
+
   backend "azurerm" {
     resource_group_name  = "compute"
     storage_account_name = "g2tfstatestorage"
@@ -11,51 +15,66 @@ terraform {
   }
 }
 
-# --- Database Module ---
+provider "azurerm" {
+  features {}
+}
+
+# --- Variable for Database (Ensure this is in your variables.tf or secret) ---
+variable "mysql_password" {
+  type      = string
+  sensitive = true
+}
+
+# --- 1. Infrastructure Resource Group ---
+resource "azurerm_resource_group" "cdn_rg" {
+  name     = "rg-cricket-cdn-prod"
+  location = "West US 2"
+}
+
+# --- 2. Database Module ---
 module "mysql_db" {
   source              = "./modules/mysql"
   server_name         = "g2-prd-mysql-wus"
-  resource_group_name = "compute"
+  resource_group_name = "compute" # Note: Ensure this RG exists or use azurerm_resource_group.cdn_rg.name
   location            = "westus3"
   admin_username      = "g2admin"
   admin_password      = var.mysql_password
   db_name             = "app_db"
 }
 
-# --- 1. Resource Group ---
-resource "azurerm_resource_group" "cdn_rg" {
-  name     = "rg-cricket-cdn-prod"
-  location = "West US 2"
-}
-
-# --- 2. Storage Account (The Origin) ---
+# --- 3. Storage Account (The Origin) ---
 resource "azurerm_storage_account" "website_storage" {
   name                     = "stparakramwebprod"
   resource_group_name      = azurerm_resource_group.cdn_rg.name
   location                 = azurerm_resource_group.cdn_rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  
+  # Required for modern Front Door access
+  allow_nested_items_to_be_public = true 
 }
 
+# Separate resource for Static Website to avoid "In-place update" issues
 resource "azurerm_storage_account_static_website" "website_static" {
   storage_account_id = azurerm_storage_account.website_storage.id
   index_document     = "index.html"
+  error_404_document = "index.html"
 }
 
-# --- 3. Azure Front Door Profile ---
+# --- 4. Azure Front Door Profile ---
 resource "azurerm_cdn_frontdoor_profile" "main" {
   name                = "afd-parakram-global"
   resource_group_name = azurerm_resource_group.cdn_rg.name
   sku_name            = "Standard_AzureFrontDoor"
 }
 
-# --- 4. Front Door Endpoint ---
+# --- 5. Front Door Endpoint ---
 resource "azurerm_cdn_frontdoor_endpoint" "endpoint" {
   name                     = "parakram-live"
   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.main.id
 }
 
-# --- 5. Origin Group and Origin ---
+# --- 6. Origin Group & Health Probe ---
 resource "azurerm_cdn_frontdoor_origin_group" "og" {
   name                     = "og-storage"
   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.main.id
@@ -65,25 +84,32 @@ resource "azurerm_cdn_frontdoor_origin_group" "og" {
   health_probe {
     interval_in_seconds = 100
     protocol            = "Https"
-    request_type        = "GET" # Standard for static sites
+    request_type        = "GET" 
   }
 }
 
+# --- 7. Origin (The "404 Killer" Configuration) ---
 resource "azurerm_cdn_frontdoor_origin" "storage_origin" {
   name                          = "origin-storage"
   cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.og.id
-  # Use primary_web_host for the static website endpoint
-  host_name                      = azurerm_storage_account.website_storage.primary_web_host
-  origin_host_header             = azurerm_storage_account.website_storage.primary_web_host
+  
+  # Points to stparakramwebprod.z1.web.core.windows.net
+  host_name                     = azurerm_storage_account.website_storage.primary_web_host
+  
+  # MANDATORY: Tells Storage Account which site we are looking for
+  origin_host_header            = azurerm_storage_account.website_storage.primary_web_host
+  
   certificate_name_check_enabled = true
+  enabled                        = true
 }
 
-# --- 6. Route (With Fix for Compression Error) ---
+# --- 8. Route (Mapping Endpoint to Origin) ---
 resource "azurerm_cdn_frontdoor_route" "route" {
   name                          = "default-route"
   cdn_frontdoor_endpoint_id     = azurerm_cdn_frontdoor_endpoint.endpoint.id
   cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.og.id
   cdn_frontdoor_origin_ids      = [azurerm_cdn_frontdoor_origin.storage_origin.id]
+  
   supported_protocols           = ["Http", "Https"]
   patterns_to_match             = ["/*"]
   forwarding_protocol           = "HttpsOnly"
@@ -92,7 +118,7 @@ resource "azurerm_cdn_frontdoor_route" "route" {
     query_string_caching_behavior = "IgnoreQueryString"
     compression_enabled           = true
 
-    # FIXED: Mandatory list when compression is enabled
+    # Full list to ensure broad support for your blog assets
     content_types_to_compress = [
       "application/eot",
       "application/font",
@@ -104,16 +130,4 @@ resource "azurerm_cdn_frontdoor_route" "route" {
       "application/pkcs7-mime",
       "application/truetype",
       "application/ttf",
-      "application/vnd.ms-fontobject",
-      "application/xhtml+xml",
-      "application/xml",
-      "text/css",
-      "text/csv",
-      "text/html",
-      "text/javascript",
-      "text/js",
-      "text/plain",
-      "text/xml"
-    ]
-  }
-}
+      "application/
