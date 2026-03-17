aec46616ff7f2cc55c3b9aff05ac0f2f434b5860
Jitendra Singh <admin@jitenrdasingh.net>
2026-03-17 16:01:20 +0530

    aks ac fix the reference variables 6

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -64,7 +64,7 @@ module "aks" {
   default_node_pool = {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_B2s"
+    vm_size    = "Standard_B2s_v2"
   }
   identity = "SystemAssigned"
 }
