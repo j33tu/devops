4f267b3e52ec866e598c97fcde553c5165f29123
Jitendra Singh <admin@jitenrdasingh.net>
2026-03-17 15:21:33 +0530

    aks ac fix

--- Terraform/modules/aks/aks.tf
+++ Terraform/modules/aks/aks.tf
@@ -1,16 +1,14 @@
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "g2k8s_Cluster"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "g2k8s"
-
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  dns_prefix          = var.dns_prefix
   default_node_pool {
-    name       = "default"
-    node_count = 2
-    vm_size    = "Standard_DS2_v2"
+    name       = var.default_node_pool.name
+    node_count = var.default_node_pool.node_count
+    vm_size    = var.default_node_pool.vm_size
   }
-
   identity {
-    type = "SystemAssigned"
+    type = var.identity
   }
 }
