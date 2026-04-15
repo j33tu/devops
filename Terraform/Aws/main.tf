fa4682bd139c0c03d08ae60db70075b5815265bf
Jitendra Singh <admin@jitenrdasingh.net>
2026-04-15 20:08:41 +0530

    adding terraform for s3 dev aws

--- Terraform/Aws/main.tf
+++ Terraform/Aws/main.tf
@@ -10,24 +10,6 @@ provider "aws" {
   region = "ap-south-2"
 }
 
-module "s3bucket" {
-  source             = "./modules/s3-bucket"
-  bucket_name_prefix = "stage"
-  location           = "ap-south-1"
-  s3bucketname       = "g2s3bucket"
-}
-module "s3bucket1" {
-  source             = "./modules/s3-bucket"
-  bucket_name_prefix = "prod"
-  location           = "ap-south-1"
-  s3bucketname       = "g2s3bucket1"
-}
-module "s3bucket2" {
-  source             = "./modules/s3-bucket"
-  bucket_name_prefix = "stg"
-  location           = "ap-south-1"
-  s3bucketname       = "g2s3bucket12"
-}
 
 
 
