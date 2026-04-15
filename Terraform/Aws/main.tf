b0675a8a52a05fc086a44923ec4f86c16e7ce672
Jitendra Singh <admin@jitenrdasingh.net>
2026-04-15 21:07:40 +0530

    aws s2 buckets

--- Terraform/Aws/main.tf
+++ Terraform/Aws/main.tf
@@ -1,1 +1,15 @@
 
+module "s3bucketprod" {
+  source             = "./modules/s3bucket"
+  bucket_name_prefix = "prod"
+  s3bucketname       = "g2bucket"
+  location           = "ap-south-1"
+
+}
+module "s3bucketstage" {
+  source             = "./modules/s3bucket"
+  bucket_name_prefix = "stage"
+  s3bucketname       = "g2bucket"
+  location           = "ap-south-1"
+
+}
