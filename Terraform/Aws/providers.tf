79d99b6a5304fcdc252d040251e8cc48fa8eb125
Jitendra Singh <admin@jitenrdasingh.net>
2026-04-15 20:20:03 +0530

    adding terraform for s3 dev aws

--- Terraform/Aws/providers.tf
+++ Terraform/Aws/providers.tf
@@ -2,11 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.40.0"
+      version = "~> 5.0"
     }
   }
 }
-
 provider "aws" {
-  # Configuration options
+  region = "ap-south-2"
 }
